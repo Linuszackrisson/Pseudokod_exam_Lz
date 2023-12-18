231f2e9a1f237667c4a77ca1422e96d746288df2
Linuszackrisson <143511885+Linuszackrisson@users.noreply.github.com>
2023-12-18 15:09:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+
+START
+
+FUNCTION named "delaUppNota(summa, antalVanner, dricksProcent)"
+    // Räkna ut notan per vän och returnera beloppet
+    SET totalBelopp = summa * (1 + dricksProcent)
+    SET beloppPerVan = totalBelopp / antalVanner
+     RETURN beloppPerVan
+END FUNCTION
+
+// Huvudprogrammet börjar här
+PRINT "Vänligen ange summan ni druckit för: "
+set summa = INPUT summa
+
+PRINT "Vänligen ange antal vänner: "
+set antalVanner = INPUT antalVanner
+
+PRINT "Vänligen ange dricks i decimalform (t.ex. 0.10 för 10%): "
+set dricksProcent = INPUT dricksProcent
+
+
+// Anropa funktionen för att räkna ut notan per vän
+set beloppPerVan = CALL FUNCTION named "delaUppNota(summa, antalVanner, dricksProcent)"
+set variabel beloppPerVan = returen från delauppnota, skicka med summa, antalvänner och dricksProcent
+// Visa resultatet
+PRINT "Varje person ska betala: " + beloppPerVan
+
+END
+
+
+
+
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+function play()
+    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
